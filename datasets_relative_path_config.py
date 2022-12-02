ffa958637a5785619446f11d39a4927a607e7a13
ankiboyz <ankiboyz@gmail.com>
2022-12-02 15:07:23 +0530

    Update datasets_relative_path_config.py

--- datasets_relative_path_config.py
+++ datasets_relative_path_config.py
@@ -3,8 +3,6 @@
 # This config is used in the get_dataset method to navigate to the actual dataset passing in the ID for the dataset.
 id_dataset_dict = {'NLP_VOCAB_BUILDER' : {'PATH': "datasets/WSJ_02-21.pos", # The path relative to the main repository, not including the starting slash.
                                           'NAME': "Wall Street Journal tagged dataset",
-                                          'DESCR': "A tagged dataset taken from the Wall Street Journal for Educational purposes. \
-                                                   "
-                              },
-                   
+                                          'DESCR': "A tagged dataset taken from the Wall Street Journal for Educational purposes."
+                              },                   
                   }
