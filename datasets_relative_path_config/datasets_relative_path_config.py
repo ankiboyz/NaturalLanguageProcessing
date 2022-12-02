09ec273e3ea3721778f9cbbddc1c86dbe70c344b
ankiboyz <ankiboyz@gmail.com>
2022-12-02 14:35:40 +0530

    Update datasets_relative_path_config.py

--- datasets_relative_path_config/datasets_relative_path_config.py
+++ datasets_relative_path_config/datasets_relative_path_config.py
@@ -2,8 +2,8 @@
 # and the relative path of the dataset to the main repository.
 # This config is used in the get_dataset method to navigate to the actual dataset passing in the ID for the dataset.
 
-id_dataset_dict = {'NLP_VOCAB_BUILDER' : {'PATH': "datasets/EAWE01.csv", # The path relative to the main repository, not including the starting slash.
-                                          'NAME': "Educational Attainment and Wage Equations (EAWE)",
+id_dataset_dict = {'NLP_VOCAB_BUILDER' : {'PATH': "datasets/WSJ_02-21.pos", # The path relative to the main repository, not including the starting slash.
+                                          'NAME': "Wall Street Journal tagged dataset",
                                           'DESCR': "A tagged dataset taken from the Wall Street Journal for Educational purposes. \
                                                    "
                               },
